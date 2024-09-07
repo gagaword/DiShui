2bbdc5e1c84353a821d97165c2b9f4378a3563a2
GaGa <fallingdust9987.1105@gmail.com>
2024-09-07 22:33:23 +0800

    导入表

--- DiShui/day27.cpp
+++ DiShui/day27.cpp
@@ -28,10 +28,14 @@ bool PritfImport(LPCSTR filePath)
 
 	PIMAGE_IMPORT_DESCRIPTOR importAddress = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD*)((BYTE*)fileBuffer + RvaToFov(peheaders.optionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress, fileBuffer)));
 
+	cout << "************************************INT****************************************" << endl;
 
-	// OriginalFirstThunk
+	// OriginalFirstThunk----INT
 	while (importAddress->OriginalFirstThunk != 0)
 	{
+		DWORD number = 0;
+		cout << "************************************DLL Info****************************************************" << endl;
+		char* dllName = (char*)((BYTE*)fileBuffer + RvaToFov(importAddress->Name, fileBuffer));
 		cout << "DLL Name: " << (char*)((BYTE*)fileBuffer + RvaToFov(importAddress->Name, fileBuffer)) << endl;
 		cout << hex << uppercase << "Original First Thunk: " << importAddress->OriginalFirstThunk << endl;
 		cout << hex << uppercase << "Original First Thunk FOA: " << RvaToFov(importAddress->OriginalFirstThunk, fileBuffer) << endl;
@@ -43,24 +47,39 @@ bool PritfImport(LPCSTR filePath)
 		while (*FunctionAddress != 0)
 		{
 			PIMAGE_IMPORT_BY_NAME pIMPORT_BY_NAME = (PIMAGE_IMPORT_BY_NAME)(DWORD*)((BYTE*)fileBuffer + RvaToFov(*FunctionAddress, fileBuffer));
-			cout << hex << "Thunk RVA: " << importAddress->OriginalFirstThunk << "  ";
-			cout << hex << "Thunk FOA: " << RvaToFov(importAddress->OriginalFirstThunk, fileBuffer) << "  ";
-			cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "  ";
-			cout << hex << "Hint: " << uppercase << pIMPORT_BY_NAME->Hint << "  ";
-			cout << "API Name: " << pIMPORT_BY_NAME->Name << endl;
+			if ((*FunctionAddress & IMAGE_ORDINAL_FLAG))
+			{
+				cout << hex << "Thunk RVA: " << importAddress->OriginalFirstThunk << "  ";
+				cout << hex << "Thunk FOA: " << RvaToFov(importAddress->OriginalFirstThunk, fileBuffer) << "  ";
+				cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "  ";
+				DWORD ordinal = *FunctionAddress & ~IMAGE_ORDINAL_FLAG32;
+				cout << "Ordinal: " << hex << uppercase << ordinal << "   ";
+				cout << "API Name: " << "NULL" << endl;
+			}
+			else {
+				cout << hex << "Thunk RVA: " << importAddress->OriginalFirstThunk << "   ";
+				cout << hex << "Thunk FOA: " << RvaToFov(importAddress->OriginalFirstThunk, fileBuffer) << "   ";
+				cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "   ";
+				cout << hex << "Hint: " << uppercase << pIMPORT_BY_NAME->Hint << "   ";
+				cout << "API Name: " << pIMPORT_BY_NAME->Name << endl;
+			}
 			FunctionAddress++;
 			importAddress->OriginalFirstThunk += sizeof(DWORD);
+			number++;
 		}
+		cout << "Number: " << dec << number << endl;
 		importAddress++;
 	}
 
-
-	// FirstThunk
-	/*while (importAddress->OriginalFirstThunk != 0)
+	cout << "*********************************IAT****************************************" << endl;
+	// FirstThunk-----IAT
+	importAddress = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD*)((BYTE*)fileBuffer + RvaToFov(peheaders.optionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress, fileBuffer)));
+	while (importAddress->OriginalFirstThunk != 0)
 	{
+		cout << "************************************DLL Info****************************************************" << endl;
 		cout << "DLL Name: " << (char*)((BYTE*)fileBuffer + RvaToFov(importAddress->Name, fileBuffer)) << endl;
-		cout << hex << uppercase << "Original First Thunk: " << importAddress->OriginalFirstThunk << endl;
-		cout << hex << uppercase << "Original First Thunk FOA: " << RvaToFov(importAddress->OriginalFirstThunk, fileBuffer) << endl;
+		cout << hex << uppercase << "First Thunk: " << importAddress->FirstThunk << endl;
+		cout << hex << uppercase << "First Thunk FOA: " << RvaToFov(importAddress->FirstThunk, fileBuffer) << endl;
 		cout << hex << uppercase << "Time/Date Stamp: " << importAddress->TimeDateStamp << endl;
 		cout << hex << uppercase << "Forwarder Chain: " << importAddress->ForwarderChain << endl;
 		cout << hex << uppercase << "Name(RVA): " << importAddress->Name << endl;
@@ -68,17 +87,28 @@ bool PritfImport(LPCSTR filePath)
 		DWORD* FunctionAddress = (DWORD*)((BYTE*)fileBuffer + RvaToFov(importAddress->FirstThunk, fileBuffer));
 		while (*FunctionAddress != 0)
 		{
-			PIMAGE_IMPORT_BY_NAME pIMPORT_BY_NAME = (PIMAGE_IMPORT_BY_NAME)(DWORD*)((BYTE*)fileBuffer + RvaToFov(*FunctionAddress, fileBuffer));
-			cout << hex << "Thunk RVA: " << importAddress->FirstThunk << "  ";
-			cout << hex << "Thunk FOA: " << RvaToFov(importAddress->FirstThunk, fileBuffer) << "  ";
-			cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "  ";
-			cout << hex << "Hint: " << uppercase << pIMPORT_BY_NAME->Hint << "  ";
-			cout << "API Name: " << pIMPORT_BY_NAME->Name << endl;
+			if (*FunctionAddress & IMAGE_ORDINAL_FLAG)
+			{
+				cout << hex << "Thunk RVA: " << importAddress->FirstThunk << "   ";
+				cout << hex << "Thunk FOA: " << RvaToFov(importAddress->FirstThunk, fileBuffer) << "   ";
+				cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "   ";
+				DWORD ordinal = *FunctionAddress & ~IMAGE_ORDINAL_FLAG32;
+				cout << "Ordinal: " << hex << uppercase << ordinal << "    ";
+				cout << "API Name: " << "NULL" << endl;
+			}
+			else {
+				PIMAGE_IMPORT_BY_NAME pIMPORT_BY_NAME = (PIMAGE_IMPORT_BY_NAME)(DWORD*)((BYTE*)fileBuffer + RvaToFov(*FunctionAddress, fileBuffer));
+				cout << hex << "Thunk RVA: " << importAddress->FirstThunk << "   ";
+				cout << hex << "Thunk FOA: " << RvaToFov(importAddress->FirstThunk, fileBuffer) << "   ";
+				cout << hex << "Thunk Value: " << uppercase << *FunctionAddress << "   ";
+				cout << hex << "Hint: " << uppercase << pIMPORT_BY_NAME->Hint << "   ";
+				cout << "API Name: " << pIMPORT_BY_NAME->Name << endl;
+			}
 			FunctionAddress++;
 			importAddress->FirstThunk += sizeof(DWORD);
 		}
 		importAddress++;
-	}*/
+	}
 	
 	return false;
 }
--- DiShui/may_day27.cpp
+++ DiShui/may_day27.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include"day27.h"
+#include"my_25.h"
 
 #define filePath "D:\\DriveLoading\\InstDrv1.exe"
+
 int main()
 {
 	PritfImport(filePath);
+
 	return 0;
 }
